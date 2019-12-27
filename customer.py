010f75a7a301e0bd2d80d12e5621e35529473c5c
Kumuditha Athukorala <55691339+Kumuditha-Athukorala@users.noreply.github.com>
2019-12-27 00:32:36 +0000

    Create customer.py
    customer class added

---
+++ customer.py
@@ -0,0 +1,17 @@
+import pyodbc
+
+class Customer:
+    def __init__(self):
+        self.__customerId=""
+        self.__customerName=""
+        self.__customerAddress=""
+        self.__customerPhoneNumber=""
+
+    def selectAllCustomers(self,cursor):
+        cursor.execute('SELECT * FROM dbo.Customer')
+        dash = '-' * 250
+        print(dash)
+        print('{:<5s}{:>30s}{:>60s}{:>30s}'.format("Id", "Name", "Address", "Phone Number"))
+        print(dash)
+        for row in cursor:
+            print('{:<5s}{:>30s}{:>60s}{:>30s}'.format(str(row[0]), row[1], row[2], row[3]))
