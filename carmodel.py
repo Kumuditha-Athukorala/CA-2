6f33f3394d013e3b47876a377901c811cf30c88d
VinitSawant09 <55744016+VinitSawant09@users.noreply.github.com>
2019-12-30 20:08:26 +0000

    Car Model
    query change to accomodate manufacturer name.

--- carmodel.py
+++ carmodel.py
@@ -11,10 +11,10 @@ class carmodel:
         self.mfid=0
 
     def searchAllCarModel(self, cursor):
-        cursor.execute('SELECT * FROM dbo.Car_Model')
+        cursor.execute('SELECT cm.car_model_id,cm.car_model_name,m.manufacturer_name,cm.car_model_price,cm.car_model_type,cm.car_model_year FROM dbo.Car_Model cm inner join  Manufacturer m on cm.manufacturer_id=m.manufacturer_id')
         dash = '-' * 150
         print(dash)
-        print('{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}'.format("Id", "Name", "Car Type", "Price", "Year"))
+        print('{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format("Id", "Car Model Name","Car Manufactuerer", "Price" ,"Car Type", "Year"))
         print(dash)
         for row in cursor:
-            print('{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}'.format(str(row[0]), row[1], row[2], row[4], row[5]))
+            print('{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format(str(row[0]), row[1], row[2], str(row[3]), str(row[4]),str(row[5])))
