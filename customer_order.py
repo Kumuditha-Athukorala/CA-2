2e77cb785d2e9356406971b794ead6298f7ac7b4
Kumuditha Athukorala <55691339+Kumuditha-Athukorala@users.noreply.github.com>
2020-01-02 23:14:11 +0000

    Customer Order
    Updated Customer Order Out puts

--- customer_order.py
+++ customer_order.py
@@ -1,5 +1,6 @@
 import pyodbc
 from customer import Customer
+from employee import employee
 
 class CustomerOrder:
 
@@ -15,15 +16,18 @@ class CustomerOrder:
     def serachAllCustomerOrders(self,cursor):
 
         cursor.execute('SELECT * FROM dbo.Customer_Order')
-        dash = '-' * 250
+        dash = '-' * 175
         print(dash)
         print('{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format("Customer Order Id", "Order Date", "Order Delivery Date", "Selling Price","Customer Id", "Employee Id" ))
         print(dash)
         for row in cursor:
-            print('{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format(str(row[0]), row[1], row[2], str(row[3]),row[4], row[5]))
+            print('{:<15s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format(str(row[0]), row[1], row[2], str(row[3]),row[4], row[5]))
 
 
     def searchOrderByCustomerId(self,cursor):
+
+        customer = Customer()
+        customer.searchCustomerByName(cursor)
 
         customerId = input("Please enter the Customer Id")
         sql =  'SELECT * FROM Customer_Order co WHERE co.customer_id =?'
@@ -31,17 +35,20 @@ class CustomerOrder:
         resultSet = cursor.fetchall()
 
         if(len(resultSet) != 0):
-            dash = '-'*200
+            dash = '-'*175
             print(dash)
             print('{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format("Customer Order Id", "Order Date", "Order Delivery Date", "Selling Price","Customer Id", "Employee Id" ))
             print(dash)
             for row in resultSet:
-                print('{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format(str(row[0]), row[1], row[2], str(row[3]),row[4], row[5]))
+                print('{:<15s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format(str(row[0]), row[1], row[2], str(row[3]),row[4], row[5]))
         else:
             print("No Customer Order with that Customer ID!")
 
 
     def searchOrderByEmpolyeeId(self,cursor):
+
+        emp = employee()
+        emp.selectBasedOnName(cursor)
 
         employeeId = input("Please enter the Employee Id")
         sql =  'SELECT * FROM Customer_Order co WHERE co.employee_id =?'
@@ -49,12 +56,12 @@ class CustomerOrder:
         resultSet = cursor.fetchall()
 
         if(len(resultSet) != 0):
-            dash = '-'*200
+            dash = '-'*175
             print(dash)
             print('{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format("Customer Order Id", "Order Date", "Order Delivery Date", "Selling Price","Customer Id", "Employee Id" ))
             print(dash)
             for row in resultSet:
-                print('{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format(str(row[0]), row[1], row[2], str(row[3]),row[4], row[5]))
+                print('{:<15s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format(str(row[0]), row[1], row[2], str(row[3]),row[4], row[5]))
         else:
             print("No Customer Order with that Employee ID!")
 
@@ -68,7 +75,11 @@ class CustomerOrder:
         self.__customerOrderDeliveryDate = input("Please Enter the Date of Order Delivered.")
         self.__customerOrderSellingPrice = int(input("Please Enter the Selling Price."))
         self.__customerId = input("Please Enter the above Customer Id.")
-        self.__employeeId = input("Please Enter the Employee Id")
+
+        emp = employee()
+        emp.selectAllEmployees(cursor)
+
+        self.__employeeId = input("Please Enter the Employee Id from the above list")
 
         database.insertCustomerOrderRecord(self.__customerOderDate, self.__customerOrderDeliveryDate, self.__customerOrderSellingPrice,
                                           self.__customerId, self.__employeeId)
@@ -88,6 +99,10 @@ class CustomerOrder:
         self.__customerOrderDeliveryDate = input("Please Enter the New or Same Date of Order Delivered.")
         self.__customerOrderSellingPrice = int(input("Please Enter the New or Same Selling Price."))
         self.__customerId = input("Please Enter the New or Same Customer Id.")
+
+        emp = employee()
+        emp.selectAllEmployees(cursor)
+        
         self.__employeeId = input("Please Enter the New or Same Employee Id")
 
         database.updateCustomerOrderRecord(self.__customerOderId,self.__customerOderDate,self.__customerOrderDeliveryDate,
