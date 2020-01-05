6deb9493d3ac3ae8db8aed7bc2a893edcac811dc
VinitSawant09 <55744016+VinitSawant09@users.noreply.github.com>
2020-01-05 19:52:05 +0000

    Try block
    incentive and inventory

--- incentive.py
+++ incentive.py
@@ -10,71 +10,80 @@ class incentive:
         self.employeeSalary = 0
 
     def selectAllIncentive(self,cursor):
-        days = int(input("Show data upto how many previous days?"))
-        cursor.execute('SELECT * FROM dbo.Incentive where incentive_date <=CONVERT (date, GETDATE())   and incentive_date>CONVERT (date, GETDATE()-?)',days)
-        dash = '-' * 100
+        try:
+            days = int(input("Show data upto how many previous days?"))
+            cursor.execute('SELECT * FROM dbo.Incentive where incentive_date <=CONVERT (date, GETDATE())   and incentive_date>CONVERT (date, GETDATE()-?)',days)
+            dash = '-' * 100
 
-        data = cursor.fetchall()
+            data = cursor.fetchall()
 
-        if len(data) != 0:
-            print(dash)
-            print('{:<5s}{:>30s}{:>30s}{:>30s}'.format("Id", "Incentive Amount", "Date", "Employee Id"))
-            print(dash)
-            for row in data:
-                print('{:<5s}{:>30s}{:>30s}{:>30s}'.format(str(row[0]), str(row[1]), row[2], row[3]))
-        else:
-            print("No incentive record present for last %i days"%days)
+            if len(data) != 0:
+                print(dash)
+                print('{:<5s}{:>30s}{:>30s}{:>30s}'.format("Id", "Incentive Amount", "Date", "Employee Id"))
+                print(dash)
+                for row in data:
+                    print('{:<5s}{:>30s}{:>30s}{:>30s}'.format(str(row[0]), str(row[1]), row[2], row[3]))
+            else:
+                print("No incentive record present for last %i days"%days)
+        except:
+            ("Something went wrong.!! Contact the administrator.!")
 
     def selectBasedOnName(self, cursor):
-        name = input("Enter name of employee. !")
-        args = ['%' + name + '%']
+        try:
+            name = input("Enter name of employee. !")
+            args = ['%' + name + '%']
 
-        cursor.execute('SELECT * FROM dbo.Employee where employee_name like ?', args)
-        dash = '-' * 180
-        data = cursor.fetchall()
-        if len(data) != 0:
-                print(dash)
-                print('{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format("Id", "Name", "Designation", "DOB", "PPS",
-                                                                         "Salary"))
-                print(dash)
-                for row in data:
-                    print(
-                        '{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format(str(row[0]), row[1], row[2], row[4], row[5],str(row[6])))
-                empid=input("Enter employee id from above.!")
-                cursor.execute("SELECT * FROM dbo.Incentive where employee_id=?", empid)
-                data = cursor.fetchall()
-                if len(data) != 0:
+            cursor.execute('SELECT * FROM dbo.Employee where employee_name like ?', args)
+            dash = '-' * 180
+            data = cursor.fetchall()
+            if len(data) != 0:
                     print(dash)
-                    print('{:<5s}{:>30s}{:>30s}{:>30s}'.format("Id", "Incentive Amount", "Date", "Employee Id"))
+                    print('{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format("Id", "Name", "Designation", "DOB", "PPS",
+                                                                             "Salary"))
                     print(dash)
-                    for row1 in data:
-                        print('{:<5s}{:>30s}{:>30s}{:>30s}'.format(str(row1[0]), str(row1[1]), row1[2], row1[3]))
-                else:
-                    print("No incentive found for the employee")
+                    for row in data:
+                        print(
+                            '{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format(str(row[0]), row[1], row[2], row[4], row[5],str(row[6])))
+                    empid=input("Enter employee id from above.!")
+                    cursor.execute("SELECT * FROM dbo.Incentive where employee_id=?", empid)
+                    data = cursor.fetchall()
+                    if len(data) != 0:
+                        print(dash)
+                        print('{:<5s}{:>30s}{:>30s}{:>30s}'.format("Id", "Incentive Amount", "Date", "Employee Id"))
+                        print(dash)
+                        for row1 in data:
+                            print('{:<5s}{:>30s}{:>30s}{:>30s}'.format(str(row1[0]), str(row1[1]), row1[2], row1[3]))
+                    else:
+                        print("No incentive found for the employee")
 
-        else:
-                print("No employee found with that name.!")
+            else:
+                    print("No employee found with that name.!")
+        except:
+            ("Something went wrong.!! Contact the administrator.!")
 
     def addIncentive(self, cursor):
-        name = input("Enter name of employee. !")
-        args = ['%' + name + '%']
+        try:
+            name = input("Enter name of employee. !")
+            args = ['%' + name + '%']
 
-        cursor.execute('SELECT * FROM dbo.Employee where employee_name like ?', args)
-        dash = '-' * 180
-        data = cursor.fetchall()
-        if len(data) != 0:
-                print(dash)
-                print('{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format("Id", "Name", "Designation", "DOB", "PPS",
-                                                                         "Salary"))
-                print(dash)
-                for row in data:
-                    print(
-                        '{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format(str(row[0]), row[1], row[2], row[4], row[5], str(row[6])))
-                empid = input("Enter employee id from above.!")
+            cursor.execute('SELECT * FROM dbo.Employee where employee_name like ?', args)
+            dash = '-' * 180
+            data = cursor.fetchall()
+            if len(data) != 0:
+                    print(dash)
+                    print('{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format("Id", "Name", "Designation", "DOB", "PPS",
+                                                                             "Salary"))
+                    print(dash)
+                    for row in data:
+                        print(
+                            '{:<5s}{:>30s}{:>30s}{:>30s}{:>30s}{:>30s}'.format(str(row[0]), row[1], row[2], row[4], row[5], str(row[6])))
+                    empid = input("Enter employee id from above.!")
 
-                db = database()
-                incentive_date = input("Enter incentive date")
-                db.insertIncentive(empid,incentive_date)
+                    db = database()
+                    incentive_date = input("Enter incentive date")
+                    db.insertIncentive(empid,incentive_date)
 
-        else:
-                print("No employee found with that name.!")
+            else:
+                    print("No employee found with that name.!")
+        except:
+            ("Something went wrong.!! Contact the administrator.!")
