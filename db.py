1ab1c4638049c3db3174bad8e1ed19c0157be624
VinitSawant09 <55744016+VinitSawant09@users.noreply.github.com>
2020-01-05 19:49:31 +0000

    try block
    employee

--- db.py
+++ db.py
@@ -4,15 +4,21 @@ import pyodbc
 class dataBase:
 
     def __init__(self):
-        self.conn = pyodbc.connect('Driver={SQL Server};'
+        try:
+            self.conn = pyodbc.connect('Driver={SQL Server};'
                              'Server=HP\\SQLEXPRESS;'
                               'Database=Over_Drive;'
                               'Trusted_Connection=yes;'
                               )
+        except:
+            ("Something went wrong in database connection.!! Contact the administrator.!")
 
     def dbConn(self):
+        try:
            cursor=self.conn.cursor()
            return cursor;
+        except:
+            ("Something went wrong in database connection.!! Contact the administrator.!")
 
 
     def insertMf(self, name, addr, eid, phone,id):
